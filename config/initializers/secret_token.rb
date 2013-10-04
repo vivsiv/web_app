68fd70ba65fe7d6f0c9b88d41f02d3fcb2d76bf0
Vivek Sivakumar <vivek@growmobile.com>
2013-10-04 15:24:02 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebApp::Application.config.secret_token = '5568ccc9bcd6d475f7caf54b240448ac348f6d8df068cd4465cb612b191644582430a80f595276855353a9c0fd89f2bcc141d9b27ab9aedd9939d0b3fbcb635d'
